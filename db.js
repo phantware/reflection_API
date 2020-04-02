d059ba8867105b14121e09e8f50c918f3499ab39
Ismail Jamiu Babatunde <32511206+phantware@users.noreply.github.com>
2020-04-02 18:11:26 +0100

    users table created (#9)

--- db.js
+++ db.js
@@ -10,15 +10,41 @@ pool.on('connect', () => {
   console.log('Connected to the database');
 });
 
-// Create table
+// Create Reflection Table
 
-const createTable = () => {
+const createReflectionTable = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
       reflections(
         id UUID PRIMARY KEY,
         success TEXT NOT NULL,
         low_point TEXT NOT NULL,
         take_away TEXT NOT NULL,
+        owner_id UUID NOT NULL,
+        created_date TIMESTAMP,
+        modified_date TIMESTAMP,
+        FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
+      )`;
+
+  pool
+    .query(queryText)
+    .then(res => {
+      console.log(res);
+      pool.end();
+    })
+    .catch(err => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+// Create User Table
+
+const createUserTable = () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS
+      users(
+        id UUID PRIMARY KEY,
+        email VARCHAR(128) UNIQUE NOT NULL,
+        password VARCHAR(128) NOT NULL,
         created_date TIMESTAMP,
         modified_date TIMESTAMP
       )`;
@@ -27,7 +53,6 @@ const createTable = () => {
     .query(queryText)
     .then(res => {
       console.log(res);
-      console.log('table created');
       pool.end();
     })
     .catch(err => {
@@ -35,10 +60,11 @@ const createTable = () => {
       pool.end();
     });
 };
-// Drop table
 
-const dropTable = () => {
-  const queryText = 'DROP TABLE IF EXISTS reflections';
+// Drop Reflection Table
+
+const dropReflectionTable = () => {
+  const queryText = 'DROP TABLE IF EXISTS reflections returning *';
   pool
     .query(queryText)
     .then(res => {
@@ -51,14 +77,48 @@ const dropTable = () => {
     });
 };
 
+// Drop User Table
+
+const dropUserTable = () => {
+  const queryText = 'DROP TABLE IF EXISTS users returning *';
+  pool
+    .query(queryText)
+    .then(res => {
+      console.log(res);
+      pool.end();
+    })
+    .catch(err => {
+      console.log(err);
+      pool.end();
+    });
+};
+
+// Create All Tables
+
+const createAllTables = () => {
+  createUserTable();
+  createReflectionTable();
+};
+
+// Drop All Tables
+
+const dropAllTables = () => {
+  dropUserTable();
+  dropReflectionTable();
+};
+
 pool.on('remove', () => {
-  console.log('Client removed');
+  console.log('client removed');
   process.exit(0);
 });
 
 module.exports = {
-  createTable,
-  dropTable
+  createReflectionTable,
+  createUserTable,
+  createAllTables,
+  dropUserTable,
+  dropReflectionTable,
+  dropAllTables
 };
 
 require('make-runnable');
